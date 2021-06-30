2827cd72fae6ef085fdb996e373da6c9dc31d4c0
marlom46 <80200977+marlom46@users.noreply.github.com>
2021-06-30 20:55:24 +0530

    Complete

--- src/components/search.js
+++ src/components/search.js
@@ -12,13 +12,19 @@ class SearchBar extends Component {
 
         return (
 
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term}
-                    onChange={event => this.setState({ term: event.target.value })} />
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>
         );
 
+    }
+
+    onInputChange = (term) => {
+
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
     }
 }
 
